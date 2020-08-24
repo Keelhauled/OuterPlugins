ea76924a49a142355f38f2391cb6258de12dd2be
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2020-08-24 14:43:49 +0300

    Add project files.

---
+++ src/DisableIntro/DisableIntro.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace DisableIntro
+{
+    [BepInPlugin("keelhauled.disableintro", "DisableIntro", "1.0.0")]
+    public class DisableIntro : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(GetType());
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(TitleScreenAnimation), "Awake")]
+        private static void Patch1(TitleScreenAnimation __instance)
+        {
+            var instance = Traverse.Create(__instance);
+            instance.Field("_gamepadSplash").SetValue(false);
+            instance.Field("_introPan").SetValue(false);
+            instance.Field("_fadeDuration").SetValue(0.001f);
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(TitleAnimationController), "Awake")]
+        private static void Patch2(TitleAnimationController __instance)
+        {
+            var instance = Traverse.Create(__instance);
+            instance.Field("_logoFadeDelay").SetValue(0.001f);
+            instance.Field("_logoFadeDuration").SetValue(0.001f);
+            instance.Field("_optionsFadeDelay").SetValue(0.001f);
+            instance.Field("_optionsFadeDuration").SetValue(0.001f);
+            instance.Field("_optionsFadeSpacing").SetValue(0.001f);
+        }
+    }
+}

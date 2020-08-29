a495b944014982fc576ea9652dca985e14dcb4e2
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2020-08-29 16:46:49 +0300

    Add low speed mode for ship

--- src/AutopilotOverride/AutopilotOverride.cs
+++ src/AutopilotOverride/AutopilotOverride.cs
@@ -10,9 +10,35 @@ namespace AutopilotOverride
     [BepInPlugin("keelhauled.autopilotoverride", "AutopilotOverride", "1.0.0")]
     public class AutopilotOverride : BaseUnityPlugin
     {
+        private static bool lowSpeedMode = false;
+        private static float lowSpeedMax = 0.49f;
+
+        private static NotificationData modeActive = new NotificationData(NotificationTarget.Ship, "LOW SPEED MODE ACTIVE", 0f, true);
+        private static NotificationData modeDeactivated = new NotificationData(NotificationTarget.Ship, "LOW SPEED MODE DEACTIVATED", 3f, true);
+
         private void Awake()
         {
             Harmony.CreateAndPatchAll(GetType());
+        }
+
+        private void Update()
+        {
+            if(OWInput.IsInputMode(InputMode.ShipCockpit) && Input.GetKeyDown(KeyCode.L))
+            {
+                lowSpeedMode = !lowSpeedMode;
+
+                if(lowSpeedMode)
+                {
+                    NotificationManager.SharedInstance.PostNotification(modeActive, true);
+                }
+                else
+                {
+                    if(NotificationManager.SharedInstance.IsPinnedNotification(modeActive))
+                        NotificationManager.SharedInstance.UnpinNotification(modeActive);
+
+                    NotificationManager.SharedInstance.PostNotification(modeDeactivated);
+                }
+            }
         }
 
         [HarmonyTranspiler, HarmonyPatch(typeof(ShipThrusterController), "ReadTranslationalInput")]
@@ -37,8 +63,15 @@ namespace AutopilotOverride
         [HarmonyPostfix, HarmonyPatch(typeof(ShipThrusterController), "ReadTranslationalInput")]
         private static void AbortAutopilot(Autopilot ____autopilot, ref Vector3 __result)
         {
-            if(____autopilot.IsFlyingToDestination() && __result.magnitude > 0f)
+            if(____autopilot.IsFlyingToDestination() && __result.magnitude > 0.1f)
                 ____autopilot.Abort();
+
+            if(lowSpeedMode)
+            {
+                __result.x = Mathf.Clamp(__result.x, -lowSpeedMax, lowSpeedMax);
+                __result.y = Mathf.Clamp(__result.y, -lowSpeedMax, lowSpeedMax);
+                __result.z = Mathf.Clamp(__result.z, -lowSpeedMax, lowSpeedMax);
+            }
         }
     }
 }
